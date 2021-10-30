4214ef29e666d226baa40ba1f88a91e10fdf8973
choisungwook <kgg1959@naver.com>
2021-10-30 12:15:31 +0900

    인메모리 사용자

--- springsecurity_app/src/main/java/com/demo/springseucurity/SpringseucurityApplication.java
+++ springsecurity_app/src/main/java/com/demo/springseucurity/SpringseucurityApplication.java
@@ -2,12 +2,34 @@ package com.demo.springseucurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 public class SpringseucurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringseucurityApplication.class, args);
+	}
+
+	@Bean
+	public UserDetailsService users() {
+		UserDetails user = User.builder()
+				.username("user")
+				.password("{noop}password")
+				.roles("USER")
+				.build();
+
+		UserDetails admin = User.builder()
+				.username("admin")
+				.password("{noop}password")
+				.roles("USER", "ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user, admin);
 	}
 
 }
